6b38b6321b5110fa95575a38ddb8e9be6d6fe4b2
zirina <JayChouZirina@gmail.com>
2022-09-24 13:23:14 +0800

    chore:init

---
+++ src/router/index.ts
@@ -0,0 +1,34 @@
+import Home from '@/views/Home/index.vue'
+import About from '@/views/About/index.vue'
+import Literature from '@/views/Literature/index.vue'
+import System from '@/views/System/index.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  linkActiveClass:'router-active',
+  routes: [
+    {
+      path:'/',
+      name:'Home',
+      component:Home
+    },
+    {
+      path:'/system',
+      name:'System',
+      component:() => System
+    },
+    {
+      path:'/literature',
+      name:'Literature',
+      component:() => Literature
+    },
+    {
+      path:'/about',
+      name:'About',
+      component:() => About
+    },
+  ]
+})
+
+export default router
